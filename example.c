ae0a0ec5dca787e39dff53be034c28e5f0f1ceff
dzwdz <kg67199@gmail.com>
2022-12-18 18:14:03 +0100

    example.c: better command syntax

--- example.c
+++ example.c
@@ -1,10 +1,22 @@
 #include "ex_cache.h"
 #include "ext2.h"
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#define errx(ret, ...) do { \
+	fprintf(stderr, __VA_ARGS__); \
+	fprintf(stderr, "\n"); \
+	exit(ret); \
+} while(0)
+
+static int my_read(void *userdata, void *buf, size_t len, size_t off);
+static int my_write(void *userdata, const void *buf, size_t len, size_t off);
+static void tree(struct ext2 *fs, uint32_t inode_n, const char *name, bool header);
+static uint32_t splitdir(struct ext2 *fs, const char *path, char **name);
 
 static int
 my_read(void *userdata, void *buf, size_t len, size_t off)
@@ -26,10 +38,130 @@ my_write(void *userdata, const void *buf, size_t len, size_t off)
 	}
 }
 
-#define TREE_HEADER "inode  perms size    name\n"
+int
+main(int argc, char **argv)
+{
+	if (argc < 2) errx(1, "no arguments");
+
+	int fd = open(argv[1], O_RDWR);
+	if (fd < 0) errx(1, "couldn't open %s", argv[1]);
+
+	/* Not part of the main library - just initializing the example caching impl from
+	 * ex_cache. */
+	struct e2device *dev = exc_init(my_read, my_write, (void*)(intptr_t)fd);
+	if (!dev) errx(1, "exc_init failed");
+
+	struct ext2 *fs = ext2_opendev(dev, exc_req, exc_drop);
+	if (!fs) errx(1, "ext2_opendev failed");
+
+	/* IO is done using "requests" - to make caching easier, instead of using
+	 * a pread/pwrite-style interface, the library asks the caching impl for a
+	 * pointer to the data, which is managed by the caching impl itself.
+	 * Most user-facing functions do this too. */
+	struct ext2d_superblock *sb = ext2_req_sb(fs);
+	if (!sb) errx(1, "couldn't get superblock");
+	printf("ext2 v%d.%d\n", sb->v_major, sb->v_minor);
+	printf("inodes: %u/%u free\n", sb->inodes_free, sb->inodes_total);
+	printf("state: %u, error handling: %u\n", sb->state, sb->error_handling);
+	printf("sizes: block %lu, frag %lu\n", fs->block_size, fs->frag_size);
+	printf("features: opt %x, ro %x, rw %x\n", sb->features_optional, sb->features_ro, sb->features_rw);
+	printf("%u block group(s)\n", fs->groups);
+	/* The library is limited to only requesting a single area at a time. However,
+	 * as the user, you can have as many requests active as your caching implementation
+	 * allows.
+	 * The example one only allows one request at a time to help catch errors in the
+	 * library. */
+	ext2_dropreq(fs, sb, false);
+
+	if (argc < 3) {
+		tree(fs, 2, "", true);
+	} else if (strcmp(argv[2], "write") == 0) {
+		if (argc < 3) errx(1, "usage: ./example write path [count]");
+		const char *path = argv[3];
+		int count = argv[4] ? atoi(argv[4]) : 0;
+
+		uint32_t n = ext2c_walk(fs, path, strlen(path));
+		if (!n) {
+			n = ext2_alloc_inode(fs, 0100700);
+			if (n == 0) {
+				errx(1, "couldn't allocate inode");
+			}
+			printf("allocated inode %u\n", n);
+
+			char *name;
+			uint32_t dir_n = splitdir(fs, path, &name);
+			if (!dir_n) {
+				errx(1, "target directory doesn't exist");
+			}
+			if (ext2_link(fs, dir_n, name, n, 0) < 0) {
+				errx(1, "couldn't create link");
+			}
+		}
+
+		{
+			struct ext2d_inode *inode;
+			inode = ext2_req_inode(fs, n);
+			if (!inode) {
+				errx(1, "couldn't read inode %u", n);
+			}
+			inode->size_lower = 0;
+			if (ext2_dropreq(fs, inode, true) < 0) {
+				errx(1, "couldn't save inode %u", n);
+			}
+		}
+
+		for (int i = 0; i < count - 1; i++) {
+			const char *s = "I can eat glass and it doesn't hurt me.\n";
+			if (ext2_write(fs, n, s, strlen(s), i * strlen(s)) <= 0) {
+				errx(1, "write error");
+			}
+		}
+	} else if (strcmp(argv[2], "link") == 0) {
+		if (argc < 4) errx(1, "usage: ./example link src target");
+		const char *src = argv[3];
+		const char *target = argv[4];
+		uint32_t src_n, target_n;
+		char *name;
+
+		target_n = splitdir(fs, target, &name);
+		if (!target_n) {
+			errx(1, "target directory doesn't exist\n");
+		}
+
+		src_n = ext2c_walk(fs, src, strlen(src));
+		if (!src_n) {
+			errx(1, "no such file\n");
+		}
+
+		if (ext2_link(fs, target_n, name, src_n, 0) < 0) {
+			errx(1, "couldn't create link\n");
+		}
+	} else if (strcmp(argv[2], "unlink") == 0) {
+		const char *path = argv[3];
+		uint32_t dir_n;
+		char *name;
+		dir_n = splitdir(fs, path, &name);
+		if (!dir_n) {
+			errx(1, "directory doesn't exist\n");
+		}
+		if (ext2_unlink(fs, dir_n, name) == 0) {
+			errx(1, "deletion failed\n");
+		}
+	} else if (strcmp(argv[2], "tree") == 0) {
+		const char *path = argv[3] ? argv[3] : "/";
+		uint32_t n = ext2c_walk(fs, path, strlen(path));
+		if (!n) {
+			errx(1, "%s not found\n", path);
+		}
+		tree(fs, n, path, true);
+	} else {
+		errx(1, "unknown command '%s'", argv[2]);
+	}
+	exc_free(dev);
+}
 
 static void
-tree(struct ext2 *fs, uint32_t inode_n, const char *name)
+tree(struct ext2 *fs, uint32_t inode_n, const char *name, bool header)
 {
 	struct ext2d_inode *inode;
 	int type;
@@ -37,6 +169,9 @@ tree(struct ext2 *fs, uint32_t inode_n, const char *name)
 	if (!inode) {
 		fprintf(stderr, "couldn't read inode %u\n", inode_n);
 		return;
+	}
+	if (header) {
+		printf("inode  perms size    name\n");
 	}
 	printf("%5u %6.3o %6u  %s\n", inode_n, inode->perms, inode->size_lower, *name ? name : "/");
 	type = (inode->perms >> 12) & 0xF;
@@ -53,7 +188,7 @@ tree(struct ext2 *fs, uint32_t inode_n, const char *name)
 			memcpy(suffix, iter.ent->name, iter.ent->namelen_lower);
 			suffix[iter.ent->namelen_lower] = '\0';
 			if (strcmp(suffix, ".") == 0 || strcmp(suffix, "..") == 0) continue;
-			tree(fs, iter.ent->inode, namebuf);
+			tree(fs, iter.ent->inode, namebuf, false);
 		}
 		free(namebuf);
 	} else if (type == 0x8) {
@@ -75,140 +210,4 @@ splitdir(struct ext2 *fs, const char *path, char **name)
 	uint32_t n = ext2c_walk(fs, path, lastslash - path);
 	*name = (void*)lastslash + 1;
 	return n;
-}
-
-int
-main(int argc, char **argv)
-{
-	(void)argc;
-	int fd = open(argv[1], O_RDWR);
-	if (fd < 0) {
-		fprintf(stderr, "couldn't open %s, quitting\n", argv[1]);
-		exit(1);
-	}
-	struct e2device *dev = exc_init(my_read, my_write, (void*)(intptr_t)fd);
-	if (!dev) {
-		fprintf(stderr, "exc_init failed\n");
-		exit(1);
-	}
-
-	/* using the cache implementation from ex_cache.c */
-	struct ext2 *fs = ext2_opendev(dev, exc_req, exc_drop);
-	if (!fs) {
-		fprintf(stderr, "ext2_opendev failed\n");
-		exit(1);
-	}
-
-	struct ext2d_superblock *sb = ext2_req_sb(fs);
-	if (!sb) {
-		fprintf(stderr, "couldn't get superblock\n");
-		exit(1);
-	}
-	printf("ext2 v%d.%d\n", sb->v_major, sb->v_minor);
-	printf("inodes: %u/%u free\n", sb->inodes_free, sb->inodes_total);
-	printf("state: %u, error handling: %u\n", sb->state, sb->error_handling);
-	printf("sizes: block %lu, frag %lu\n", fs->block_size, fs->frag_size);
-	printf("features: opt %x, ro %x, rw %x\n", sb->features_optional, sb->features_ro, sb->features_rw);
-	printf("%u block group(s)\n", fs->groups);
-	ext2_dropreq(fs, sb, false);
-
-	printf(TREE_HEADER);
-	if (argc < 3) {
-		tree(fs, 2, "");
-	}
-	for (int arg = 2; arg < argc; arg++) {
-		const char *path = argv[arg];
-		uint32_t n;
-		if (path[0] == '+') { /* write some stuff to the file */
-			int count = 0;
-			for (; *path == '+'; path++) count++;
-			n = ext2c_walk(fs, path, strlen(path));
-			if (!n) {
-				n = ext2_alloc_inode(fs, 0100700);
-				if (n == 0) {
-					printf("couldn't allocate inode\n");
-					continue;
-				}
-				printf("allocated inode %u\n", n);
-
-				char *name;
-				uint32_t dir_n = splitdir(fs, path, &name);
-				if (!dir_n) {
-					fprintf(stderr, "target directory doesn't exist\n");
-					continue;
-				}
-				if (ext2_link(fs, dir_n, name, n, 0) < 0) {
-					fprintf(stderr, "couldn't create link\n");
-					continue;
-				}
-			}
-
-			{
-				struct ext2d_inode *inode;
-				inode = ext2_req_inode(fs, n);
-				if (!inode) {
-					fprintf(stderr, "couldn't read inode %u\n", n);
-					continue;
-				}
-				inode->size_lower = 0;
-				if (ext2_dropreq(fs, inode, true) < 0) {
-					fprintf(stderr, "couldn't save inode %u\n", n);
-					// TODO mark fs as dirty
-					continue;
-				}
-			}
-
-			for (int i = 0; i < count - 1; i++) {
-				const char *s = "I can eat glass and it doesn't hurt me.\n";
-				if (ext2_write(fs, n, s, strlen(s), i * strlen(s)) <= 0) {
-					fprintf(stderr, "write error :(\n");
-					break;
-				}
-			}
-			tree(fs, n, path);
-		} else if (strchr(path, ':')) { /* source:linktarget */
-			uint32_t src_n, target_n;
-			char *target, *name;
-			target = strchr(path, ':') + 1;
-			target[-1] = '\0';
-
-			target_n = splitdir(fs, target, &name);
-			if (!target_n) {
-				fprintf(stderr, "target directory doesn't exist\n");
-				continue;
-			}
-
-			src_n = ext2c_walk(fs, path, strlen(path));
-			if (!src_n) {
-				fprintf(stderr, "no such file\n");
-				continue;
-			}
-
-			if (ext2_link(fs, target_n, name, src_n, 0) < 0) {
-				fprintf(stderr, "couldn't create link\n");
-			} else {
-				tree(fs, target_n, target);
-			}
-		} else if (strchr(path, '-')) { /* unlink */
-			uint32_t dir_n;
-			char *name;
-			dir_n = splitdir(fs, path + 1, &name);
-			if (!dir_n) {
-				fprintf(stderr, "directory doesn't exist\n");
-				continue;
-			}
-			if (ext2_unlink(fs, dir_n, name) == 0) {
-				fprintf(stderr, "deletion failed\n");
-				continue;
-			}
-		} else { /* default: show tree at path */
-			n = ext2c_walk(fs, path, strlen(path));
-			if (!n) {
-				printf("%s not found\n", path);
-				continue;
-			}
-			tree(fs, n, path);
-		}
-	}
-	exc_free(dev);
 }
